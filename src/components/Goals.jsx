942704eb830de66680c05422b8f25e1975a2744c
annpermi <perminova.anna85@gmail.com>
2021-05-25 23:13:53 -0400

    Router

---
+++ src/components/Goals.jsx
@@ -0,0 +1,8 @@
+
+const Goals = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default Goals;
---
+++ src/components/Vision.jsx
@@ -0,0 +1,8 @@
+
+const Vision = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default Vision;
