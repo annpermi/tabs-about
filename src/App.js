a3cb4182dfc57d6a739b6701b0667cbe519f6aef
annpermi <perminova.anna85@gmail.com>
2021-05-25 23:11:32 -0400

    Created Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import History from './components/History';
+import Vision from './components/Vision';
+import Goals from './components/Goals';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className="container">
+        <div className="center">
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, aperiam!</p>
+        </div>
+
+        <div className="middle about-center">
+            <div className="img-container">
+                <img src="https://vannilla-js-basic-project-11-tabs.netlify.app/hero-bcg.jpeg" alt=""/>
+            </div>
+            <div className="section-btn">
+                <div className="buttons">
+                    <Link to='/' className='active'>History</Link>
+                    <Link to='/vision'>Vision</Link>
+                    <Link to='/goals'>Goals</Link>
+                </div>
+                <div id="box">
+                    <ul id="wrapper">
+                    <Switch>
+                      <Route exact path='/' components={History}/>
+                      <Route path='/vision' components={Vision}/>
+                      <Route path='/goals' components={Goals}/>
+                    </Switch>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
+    </Router>
   );
 }
 
